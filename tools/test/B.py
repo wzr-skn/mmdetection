c4ff1c7d57733f6010b4ceb7fc1a5662c0ace862
HAOCHENYE <21724054@zju.edu.cn>
2021-08-17 15:53:01 +0800

    update export onnx and sandnet, modify DBB, TODO modify repvgg

--- tools/test/B.py
+++
@@ -1,8 +0,0 @@
-import random
-random.seed(10086)
-def funcB():
-    a = [random.randint(0, 9) for _ in range(3)]
-    b = [random.randint(0, 9) for _ in range(3)]
-    c = [random.randint(0, 9) for _ in range(3)]
-    d = [random.randint(0, 9) for _ in range(3)]
-    return
